e8aed3213428eeb1377ac483d6e0507c2d5a0484
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-28 16:07:55 -0600

    se añadio la conexión de mongodb utilizando mongoose

--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const cors = require('cors')
+const { dbConnection } = require('../database/config')
 
 class Server {
 
@@ -9,6 +10,8 @@ class Server {
         this.port = process.env.PORT
         this.usuariosPath = '/api/usuarios'
 
+        //Conectar a base de datos
+        this.conectarDB()
 
         //Middlewares
         this.middlewares()
@@ -16,6 +19,10 @@ class Server {
         //rutas de mi aplicación
         this.routes()
 
+    }
+
+    async conectarDB() {
+        await dbConnection()
     }
 
     middlewares() {
