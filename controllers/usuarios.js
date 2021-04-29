f6006efeb8377a85b336e34d832102a83bb1385a
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-29 14:20:25 -0600

    desestructuracion totales y filtro por estados obteniendo información por promesa para optimizar llamadas sincronas

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -9,10 +9,25 @@ const Usuario = require('../models/usuario')
 const usuariosGet = async (req = request, res = response) => {
 
     const { limite = 5, desde = 0 } = req.query;
-    const usuarios = await Usuario.find()
+    const filtro = { estado: true }
+
+    /*REF
+     OPTIMIZACION correr en paralelo para que esto no corra en serie sino en paralelo
+    const usuarios = await Usuario.find(filtro) //esto corre en serie
         .skip(Number(desde))
         .limit(Number(limite))
+
+    const total = await Usuario.countDocuments(filtro) //esto corre en serie
+    */
+
+    //se desestructura posicionalmente el resultado de las promesas
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(filtro),
+        Usuario.find(filtro).skip(Number(desde)).limit(Number(limite))
+    ])
+
     res.json({
+        total,
         usuarios
     })
 
