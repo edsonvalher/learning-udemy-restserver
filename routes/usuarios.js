9ff625bbac8f89fbc1457aaa5ee34717ceec1c25
Edson Valenzuela <edsonvalher@gmail.com>
2021-04-28 18:24:24 -0600

    validacion directa en base de datos buscando mediante el modelo role en la coleccion (tabla) llamada roles

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,8 @@
 const { Router } = require('express')
 const { usuariosGet, usuariosPost, usuariosPut, usuariosDelete, usuariosPatch } = require('../controllers/usuarios')
 const { check } = require('express-validator')
+const Role = require('../models/role')
+
 const { validarCampos } = require('../middlewares/validar-campos')
 
 const router = Router()
@@ -15,7 +17,14 @@ router.post('/',
         check('nombre', 'nombre es obligatorio').not().isEmpty(), //not es para que valide que no tiene que venir vacio
         check('correo', 'correo no valido').isEmail(),
         check('password', 'password mas de 6 letras').isLength({ min: 6 }),
-        check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+        //check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+        check('rol').custom(async (rol = '') => {
+            const existeRol = await Role.findOne({ rol })
+            if (!existeRol) {
+                throw new Error(`no existe rol ${rol} en base de datos`)
+            }
+
+        }),
         validarCampos
     ]
     , usuariosPost)
