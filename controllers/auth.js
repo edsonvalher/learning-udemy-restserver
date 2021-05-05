b59cff0dc3e7bc19f68f64eb34f3464a7c6d6304
Edson Valenzuela <edsonvalher@gmail.com>
2021-05-05 15:20:11 -0600

    se agrego validaciones para registrar cliente y para validar el status

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,8 +48,40 @@ const googleSignIn = async (req = request, res = response) => {
     const { id_token } = req.body
 
     try {
-        const googleUser = await googleVerify(id_token)
-        console.log(googleUser)
+        const { correo, nombre, img } = await googleVerify(id_token)
+
+        let usuario = await Usuario.findOne({ correo })
+
+        if (!usuario) {
+            //tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+
+            }
+            usuario = new Usuario(data)
+            await usuario.save()
+        }
+        //si el usuario en base de datos tiene el estado en false 
+        if (!usuario.estado) {
+            return res.status(401).json(
+                {
+                    msg: 'Usuario está bloqueado'
+                }
+            )
+        }
+
+        const token = await generarJWT(usuario.id)
+        res.json(
+            {
+                usuario,
+                token
+            }
+        )
+
         res.json(
             {
                 msg: "Todo ok!",
