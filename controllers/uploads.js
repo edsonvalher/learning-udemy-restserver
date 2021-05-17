f38f396e13dc3fc394cacc121587c04b6a03a362
Edson Valenzuela <edsonvalher@gmail.com>
2021-05-17 16:04:54 -0600

    se agrego la configuración de CLOUDINARY en las variables de entorno y el funciomiento nuevo en el controlador

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const fs = require('fs')
 
+const claudinary = require('cloudinary').v2
+
+claudinary.config(process.env.CLOUDINARY_URL)
+
 const { response, request } = require('express');
 const { subirArchivo } = require('../helper');
 
@@ -141,8 +145,71 @@ const mostrarImagen = async (req = request, res = response) => {
 
 }
 
+
+const actualizarImagenCloudinary = async (req = request, res = response) => {
+
+    const { id, coleccion } = req.params
+
+    let modelo
+    switch (coleccion) {
+
+        case 'usuarios':
+
+            modelo = await Usuario.findById(id)
+            if (!modelo) {
+                return res.status(400).json(
+                    {
+                        msg: `No existe un usuario con el id ${id}`
+                    }
+                )
+            }
+
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id)
+            if (!modelo) {
+                return res.status(400).json(
+                    {
+                        msg: `No existe un producto con el id ${id}`
+                    }
+                )
+            }
+
+            break;
+
+        default:
+            return res.status(500).json(
+                {
+                    msg: 'Se me olvidó validar esto'
+                }
+            )
+    }
+
+    //limpiar imagenes previas
+    if (modelo.img) {
+        //borrar imagen de servidor cloud
+    }
+    const { tempFilePath } = req.files.archivo
+    const { secure_url } = await claudinary.uploader.upload(tempFilePath)
+
+    modelo.img = secure_url
+
+    await modelo.save()
+
+
+    res.json(
+        {
+            modelo
+        }
+    )
+
+
+}
+
 module.exports = {
     cargarArchivo,
     actualizarImagen,
-    mostrarImagen
+    mostrarImagen,
+    actualizarImagenCloudinary
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads')
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require('../controllers/uploads')
 const { validarCampos, validarArchivo } = require('../middlewares')
 const { coleccionesPermitidas } = require('../helper')
 
@@ -13,7 +13,7 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe ser de mongo').isMongoId(),
     check('coleccion').custom(c => coleccionesPermitidas(c, ['usuarios', 'productos'])),
     validarCampos
-], actualizarImagen)
+], actualizarImagenCloudinary) //actualizarImagen)
 
 router.get('/:coleccion/:id', [
     check('id', 'El id debe ser de mongo').isMongoId(),
